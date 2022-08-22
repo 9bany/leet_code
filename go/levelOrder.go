a234668130f82042801465a1176574e2515b4bb2
9bany <bany.y0599@gmail.com>
2022-08-22 21:45:33 +0700

    levelOrder

---
+++ go/levelOrder.go
@@ -0,0 +1,24 @@
+package go
+
+func levelOrder(root *TreeNode) [][]int { 
+    result := [][]int{}
+    
+    var dns func(*TreeNode, int)
+    
+    dns = func(root *TreeNode, level int) {
+        if root == nil {
+            return 
+        }
+        
+        if level >= len(result) {
+            result = append(result, []int{})
+        }
+        
+        result[level] = append(result[level], root.Val)
+		dns(root.Left, level+1)
+		dns(root.Right, level+1)
+        
+    }
+    dns(root, 0)
+    return result
+}
