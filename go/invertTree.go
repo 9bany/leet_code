65905516d922437c350f8e6aabbe068067e7b6a7
9bany <bany.y0599@gmail.com>
2022-08-22 21:44:11 +0700

    invertTree.go

---
+++ go/invertTree.go
@@ -0,0 +1,17 @@
+package go
+
+func invertTree(root *TreeNode) *TreeNode {
+    invert(root)
+    return root
+}
+
+func invert(root *TreeNode) {
+    if root != nil {
+        tmp := root.Left
+        root.Left = root.Right
+        root.Right = tmp
+
+        invert(root.Left)
+        invert(root.Right)
+    }
+}
