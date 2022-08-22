e07b189fcdd13ef701231b209b09697b18224fca
9bany <bany.y0599@gmail.com>
2022-08-22 21:48:13 +0700

    inorderTraversal

---
+++ go/inorderTraversal.go
@@ -0,0 +1,14 @@
+package go
+
+func inorderTraversal(root *TreeNode) []int {
+    res := []int{}
+    var inorder func(root *TreeNode)
+    inorder = func(root *TreeNode) {
+        if root == nil { return }
+        inorder(root.Left)
+        res = append(res, root.Val)
+        inorder(root.Right)
+    }
+    inorder(root)
+    return res
+}
