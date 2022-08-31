e9a167c8c729cfb275e3add7629dc95b264c80b2
9bany <bany.y0599@gmail.com>
2022-08-31 13:27:43 +0700

    update searchMatrix

--- go/searchMatrix.go
+++ go/searchMatrix.go
@@ -1,18 +1,24 @@
 package main
 
 func searchMatrix(matrix [][]int, target int) bool {
-	if len(matrix) == 0 { return false }
-	
-	for i := 0; i < len(matrix); i++ {
-		if target == matrix[i][0] { return true }
+    if len(matrix) == 0 { return false } 
     
-		if target > matrix[i][0] {
-					
-			for j := 0; j < len(matrix[i]); j++ {
-				if target == matrix[i][j] { return true }
-			}
-		}
-	}
-	
-	return false    
+    for index, row := range matrix {
+        
+        if target >= row[0] {
+            
+            for _, value := range matrix[index] {
+                
+                if value > value {
+                    break
+                }
+                
+                if target == value {
+                    return true
+                }
+            }
+        }
+    }
+    
+    return false
 }
