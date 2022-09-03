84a3ea5308d248cc598d63c42a3bda06392ecd8c
9bany <bany.y0599@gmail.com>
2022-09-03 10:53:22 +0700

    revert int

---
+++ go/revertInt.go
@@ -0,0 +1,14 @@
+package main
+
+func reverse(x int) int {
+    result := 0
+	for x != 0 {
+		result = result*10 + x%10
+		x = x / 10
+	}
+    
+	if result > 1<<31-1 || result < -(1<<31) {
+		return 0
+	}
+	return result
+}
